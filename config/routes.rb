b2cf3e53ff459d4b944e4fa752306147ecf79aee
Fady <fady31@hotmail.com>
2016-03-18 02:35:03 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  get 'companies/index'
-  root 'companies#index'
+  get 'welcome/index'
+  root 'welcome#index'
   
   #oauth2 to access github api
   get 'auth/github', :as => 'github_auth'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
