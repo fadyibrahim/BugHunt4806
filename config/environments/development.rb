bde8f809c9db8f6f06955c18d0a9c97006913366
Luke Morrison <lukemo132@outlook.com>
2016-03-24 09:48:14 -0400

    Re-included the assets/application.css to the public facing directory

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,9 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+config.serve_static_files = true
+config.assets.compile = true
+config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
