492c796f923d0a4114db9abf3192c476d4ff65d3
strangestar <s.macdonald@null.net>
2016-03-31 11:11:03 -0400

    changed pluralization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 	belongs_to :company
-	has_many :created_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_creations
-	has_many :assigned_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_assignmentss
-	has_many :resolved_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_resolutions
+	has_many :created_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_creation
+	has_many :assigned_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_assignment
+	has_many :resolved_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_resolution
  	has_secure_password
 	validates :email, uniqueness: true
 end
