ae98839be05b01c6085158b92cd5ccb46bdac19e
strangestar <s.macdonald@null.net>
2016-03-31 11:14:15 -0400

    added more relations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
 	belongs_to :company
-	has_many :created_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_creation
-	has_many :assigned_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_assignment
-	has_many :resolved_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_resolution
+	has_many :bug_creations
+	has_many :bug_assignments
+	has_many :bug_resolutions
+	has_many :created_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_creations
+	has_many :assigned_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_assignments
+	has_many :resolved_bugs, :class_name => 'Bug', :foreign_key => 'bug_id', :through => bug_resolutions
  	has_secure_password
 	validates :email, uniqueness: true
 end
